be4183fa00b898d3f6356549eb5f83539123be35
Avanthika210 <avantika@example.com>
2025-07-31 20:18:31 +0530

    Add Java program

--- StringCompression.java
+++ StringCompression.java
@@ -7,24 +7,19 @@ public class StringCompression {
     }
 
     public static String compressString(String str) {
-        StringBuilder result = new StringBuilder();
-
+        String result = "";  
         int count = 1;
         for (int i = 1; i < str.length(); i++) {
-         
+
             if (str.charAt(i) == str.charAt(i - 1)) {
                 count++;
             } else {
-                result.append(str.charAt(i - 1)).append(count);
-                count = 1; 
+                result = result + str.charAt(i - 1) + count;
+                count = 1;
             }
         }
+        result = result + str.charAt(str.length() - 1) + count;
 
-       
-        result.append(str.charAt(str.length() - 1)).append(count);
-
-        return result.toString();
+        return result;
     }
 }
-
-
